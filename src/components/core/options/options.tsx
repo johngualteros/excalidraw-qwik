23b7e19b850a735c21b42c9801deab4f4403e45a
john <gualterosjohn40@gmail.com>
2023-11-20 19:16:21 -0500

    feat: adding the icons for options

--- src/components/core/options/options.tsx
+++ src/components/core/options/options.tsx
@@ -1,5 +1,6 @@
 import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import styles from './options.css?inline';
+import { LuCaseSensitive, LuCircle, LuDiamond, LuHand, LuImage, LuMinus, LuMousePointer2, LuMoveRight, LuPencil, LuSquare } from '@qwikest/icons/lucide';
 
 interface OptionsProps {
     backgroundColor: string;
@@ -14,7 +15,46 @@ export const Options = component$(({backgroundColor}: OptionsProps) => {
             }}
             class="container"
         >
-            <h1>Options</h1>
+            <div class="container_option">
+                <LuHand class="icon"/>
+                <span class="badge">0</span>
+            </div>
+            <div class="container_option">
+                <LuMousePointer2 class="icon"/>
+                <span class="badge">1</span>
+            </div>
+            <div class="container_option">
+                <LuSquare class="icon"/>
+                <span class="badge">2</span>
+            </div>
+            <div class="container_option">
+                <LuDiamond class="icon"/>
+                <span class="badge">3</span>
+            </div>
+            <div class="container_option">
+                <LuCircle class="icon"/>
+                <span class="badge">4</span>
+            </div>
+            <div class="container_option">
+                <LuMoveRight class="icon"/>
+                <span class="badge">5</span>
+            </div>
+            <div class="container_option">
+                <LuMinus class="icon"/>
+                <span class="badge">6</span>
+            </div>
+            <div class="container_option">
+                <LuCaseSensitive class="icon"/>
+                <span class="badge">7</span>
+            </div>
+            <div class="container_option">
+                <LuPencil class="icon"/>
+                <span class="badge">8</span>
+            </div>
+            <div class="container_option">
+                <LuImage class="icon"/>
+                <span class="badge">9</span>
+            </div>
         </div>
     );
 });
