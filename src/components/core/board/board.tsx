0083b5e35255cea041305ad44fe17cd237a1ddfd
john <gualterosjohn40@gmail.com>
2023-12-14 19:18:46 -0500

    breaking changes

--- src/components/core/board/board.tsx
+++ src/components/core/board/board.tsx
@@ -1,4 +1,4 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { $, component$, useStylesScoped$ } from "@builder.io/qwik";
 import styles from './board.css?inline';
 
 import { useTheme } from "~/hooks/useTheme";
@@ -10,10 +10,26 @@ export const Board =  component$(() => {
     useStylesScoped$(styles);
     const { darkBackgroundColor } = useTheme();
 
+    const drawLine = $(() => {
+        const canvas: any = document.getElementById('canvas');
+        const context = canvas?.getContext('2d');
+
+        context.beginPath();
+        context.moveTo(400, 400);
+        context.lineTo(400, 400);
+        context.strokeStyle = 'white';
+        context.lineWidth = 2;
+        context.stroke();
+        context.closePath();
+    });
+
+    drawLine();
+
     return (
         <main id="board">
             <DropDown backgroundColor={darkBackgroundColor.value} />
             <Options backgroundColor={darkBackgroundColor.value} />
+            <canvas id="canvas" width={500} height={400}></canvas>
         </main>
     );
 });
